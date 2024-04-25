502d254f8fee096c94894547781b17cbab0a6246
luisale06 <luisale061001@gmail.com>
2024-04-25 01:52:46 -0600

    Restricciones
    Se refinaron algunas restricciones y se comenzó a implementar la interacción entre los pisos

--- Prototipo Interfaz/script.js
+++ Prototipo Interfaz/script.js
@@ -16,9 +16,10 @@ let currentTop = 0;
 
 
 let pasoDeMovimiento = 170;
-let isClosed = 0; //0 abierta, 1 cerrada
+let isClosed = 1; //0 abierta, 1 cerrada
 let isDoorMoving = 0; //0 quieta, 1 moviendo
 let isElevMoving = 0; //0 quieto, 1 moviendo
+let floorAmount = 0; //Inicia en 0, se incrementa conforme se añaden pisos
 
 let x = 0;
 let y = 0;
@@ -49,76 +50,47 @@ function loadPage(){
 
     if (btnCerrar) { //Cierra ambas puertas manualmente
         btnCerrar.addEventListener('click', function() {
-            if (isDoorMoving == 0){
+            if (isDoorMoving == 0 && isClosed == 0){
                 cerrarPuertas();
             }
         });
       }
     if (btnAbrir) { //Abre ambas puertas manualmente
         btnAbrir.addEventListener('click', function(){
-            if (isDoorMoving == 0){
+            if (isDoorMoving == 0 && isClosed == 1){
                 abrirPuertas();
             }
         })
     }
     if (btnP1) { //Selecciona el piso 1
         btnP1.addEventListener('click', function(){
-            if (isClosed == 0 && isDoorMoving == 0 && isElevMoving == 0){
-                cerrarPuertas();
-                setTimeout(function() {
-                    subebajaElevador(1);
-                }, 1000);
-            }
-            else if (isClosed == 1 && isDoorMoving == 0 && isElevMoving == 0){
-                subebajaElevador(1);
-            }
+            llegarPisoSeleccionado(1);
         })
     }
     if (btnP2) { //Selecciona el piso 2
         btnP2.addEventListener('click', function(){
-            if (isClosed == 0 && isDoorMoving == 0 && isElevMoving == 0){
-                cerrarPuertas();
-                setTimeout(function() {
-                    subebajaElevador(2);
-                }, 1000);
-            }
-            else if (isClosed == 1 && isDoorMoving == 0 && isElevMoving == 0){
-                subebajaElevador(2);
-            }
+            llegarPisoSeleccionado(2);
         })
     }
     if (btnP3) { //Selecciona el piso 3
         btnP3.addEventListener('click', function(){
-            if (isClosed == 0 && isDoorMoving == 0 && isElevMoving == 0){
-                cerrarPuertas();
-                setTimeout(function() {
-                    subebajaElevador(3);
-                }, 1000);
-            }
-            else if (isClosed == 1 && isDoorMoving == 0 && isElevMoving == 0){
-                subebajaElevador(3);
-            }
+            llegarPisoSeleccionado(3);
         })
     }
     if (btnP4) { //Selecciona el piso 4
         btnP4.addEventListener('click', function(){
-            if (isClosed == 0 && isDoorMoving == 0 && isElevMoving == 0){
-                cerrarPuertas();
-                setTimeout(function() {
-                    subebajaElevador(4);
-                }, 1000);
-            }
-            else if (isClosed == 1 && isDoorMoving == 0 && isElevMoving == 0){
-                subebajaElevador(4);
-            }
+            llegarPisoSeleccionado(4);
         })
     }
     if (btnAddFloor) { 
-        btnAddFloor.addEventListener('click', addFloor)
+        btnAddFloor.addEventListener('click', function(){
+            floorAmount += 1;
+            addFloor(floorAmount);
+        })
     }
-}
+};
 
-function addFloor(){        
+function addFloor(numpiso){        
         
     const elevator = document.createElement("div");    
     elevator.className = "elevador";
@@ -142,29 +114,37 @@ function addFloor(){
     const rightDoor = document.createElement("span");
     rightDoor.innerHTML = "<img draggable=\"false\" src=\"./icons/rightDoor.png\" height=\"520\" width=\"170\"/>";
     rightDoor.className = "puerta puerta-der";
-    rightDoor.style.top = `${130}px`;    
+    rightDoor.style.top = `${130}px`;
+    
+    //Número de piso
+    const floorNumber = document.createElement("span");
+    floorNumber.textContent = `P${numpiso}`;
+    floorNumber.className = "floor-number";
+    floorNumber.style.top = `${60}px`;
     
     elevator.appendChild(outElevator);
     elevator.appendChild(inElevator);
     elevator.appendChild(leftDoor);
     elevator.appendChild(rightDoor);
+    elevator.appendChild(floorNumber);
        
     floorList.push(elevator);
     elevatorContainer.appendChild(elevator);
     currentTop += 850;
-    elevatorContainer.style.height = `${currentTop}px`;    
+    elevatorContainer.style.height = `${currentTop}px`; 
     console.log(y);
-}
+    console.log(floorNumber);
+};
 
 function moverPuertasDer(puerta) {        
     const posicionActual = parseInt(getComputedStyle(puerta).left);
     puerta.style.left = `${posicionActual + pasoDeMovimiento}px`;
-  }
+};
 
 function moverPuertasIzq(puerta) {
     const posicionActual = parseInt(getComputedStyle(puerta).left);
     puerta.style.left = `${posicionActual - pasoDeMovimiento}px`;
-}
+};
 
 function abrirPuertas(){
     const index = y/850;
@@ -179,7 +159,8 @@ function abrirPuertas(){
         isClosed = 0;
         isDoorMoving = 0;
     }, 1000);
-}
+};
+
 function cerrarPuertas(){
     const index = y/850;
     console.log(index, y);
@@ -193,7 +174,7 @@ function cerrarPuertas(){
         isClosed = 1;
         isDoorMoving = 0;
     }, 1000);
-}
+};
 
 function subebajaElevador(piso){
     isElevMoving = 1;
@@ -202,14 +183,27 @@ function subebajaElevador(piso){
         isElevMoving = 0;
         abrirPuertas();
     }, 1000);
-}
+};
+
+//Función que mueve el elevador al piso seleccionado en el panel interno
+function llegarPisoSeleccionado(piso){
+    if (isClosed == 0 && isDoorMoving == 0 && isElevMoving == 0){
+        cerrarPuertas();
+        setTimeout(function() {
+            subebajaElevador(piso);
+        }, 1000);
+    }
+    else if (isClosed == 1 && isDoorMoving == 0 && isElevMoving == 0){
+        subebajaElevador(piso);
+    }
+};
 
 window.addEventListener("load", loadPage);
 
 function showCoords(event) {
     x = event.clientX;   
     y = event.pageY; 
-}
+};
 
 document.addEventListener("scroll", (event)=>{
     x = event.clientX;   
